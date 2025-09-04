5c724c1f2b647a879ee010b657da80084d5ab74c
Martin Castro <martin1313jmc@gmail.com>
2025-09-04 09:59:42 -0300

    cambio final?

--- src/middleware/PublicidadValidacion.mjs
+++ src/middleware/PublicidadValidacion.mjs
@@ -7,7 +7,4 @@ export const validarPublicidad = () => [
     body('descripcion')
         .notEmpty().withMessage('La descripción es requerida.')
         .isString({min: 15, max: 250}).withMessage('La descripción debe ser una cadena de texto.'),
-    body('idDoctor')
-        .notEmpty().withMessage('El ID del doctor es requerido.')
-        .isMongoId().withMessage('El ID del doctor debe ser un ID de MongoDB válido.')
 ];
